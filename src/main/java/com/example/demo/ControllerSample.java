c099b5f38c501a2eb9f6a71ab74b1aa67d009629
Rafael Nanes <rafaelbraga_22@hotmail.com>
2026-03-20 15:24:26 +0000

    initial commit

--- src/main/java/com/example/demo/ControllerSample.java
+++ src/main/java/com/example/demo/ControllerSample.java
@@ -4,17 +4,61 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Duration;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/sample")
 public class ControllerSample {
 
-  @GetMapping
-  public String sample() {
-    ExecutorService executorService = Executors.newSingleThreadExecutor();
-    return "Hello World";
+  @GetMapping("/single-thread-executor")
+  public void singleThreadExecutor() {
+    ExecutorService executor = Executors.newSingleThreadExecutor();
+    executor.submit(sampleTask());
+    executor.shutdown();
+  }
+
+  @GetMapping("/cached-thread-pool")
+  public void cachedThreadPool() {
+    ExecutorService executor = Executors.newCachedThreadPool();
+    executor.submit(sampleTask());
+    executor.shutdown();
+  }
+
+  @GetMapping("/fixed-thread-pool")
+  public void fixedThreadPool() {
+    ExecutorService executor = Executors.newFixedThreadPool(4);
+    executor.submit(sampleTask());
+    executor.shutdown();
+  }
+
+  @GetMapping("/scheduled-thread-pool")
+  public void scheduledThreadPool() {
+    var executor = Executors.newScheduledThreadPool(4);
+    executor.submit(sampleTask());
+    executor.shutdown();
+  }
+
+  @GetMapping("/thread-pool-executor")
+  public void threadPoolExecutor() {
+    ExecutorService executor = new ThreadPoolExecutor(
+        4, 6, 60L, TimeUnit.SECONDS, new LinkedBlockingQueue<>(200));
+    executor.submit(sampleTask());
+    executor.shutdown();
+  }
+
+  private Runnable sampleTask() {
+    return () -> {
+      try {
+        Thread.sleep(Duration.ofMillis(500));
+      } catch (InterruptedException e) {
+        throw new RuntimeException(e);
+      }
+    };
   }
 
 }
